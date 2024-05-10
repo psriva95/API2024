7af4453fa6badb2fd6c0bbef2bcaa4d1e9dda772
psriva95 <poojasrivastava95@outlook.com>
2024-05-10 11:55:05 +0530

    Updating Project Libraries

--- src/test/java/TestAPIBasicFlows/BaseTest.java
+++ src/test/java/TestAPIBasicFlows/BaseTest.java
@@ -1,4 +1,37 @@
 package TestAPIBasicFlows;
 
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 public class BaseTest {
+
+    @Test
+    public void testGetResponseStatusCode(){
+        given()
+                .baseUri("https://api.postman.com")
+                .header("x-api-key","").
+        when()
+                .get("/workspaces").
+        then()
+                .log().all().
+                assertThat().
+                statusCode(200);
+    }
+
+    @Test
+    public void testGetResponseBody(){
+        given()
+                .baseUri("https://api.postman.com")
+                .header("x-api-key","").
+                when()
+                .get("/workspaces").
+                then()
+                .log().all().
+                assertThat().
+                statusCode(200)
+                .body("workspaces.name",hasItems("My Workspace"));
+    }
+
 }
